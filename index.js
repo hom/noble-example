57f6b9c0287c97ffc41f80d08a9f73195e87541e
mengj <813935532@qq.com>
2021-06-27 11:52:25 +0800

    feat: add discover service uuid

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const noble = require('noble')
-noble.startScanning()
+noble.startScanning(['6e400001b5a3f393e0a9e50e24dcca9e'], false, (error) => {
+  if (error) {
+    return console.error(error)
+  }
+})
+
+// noble.startScanning()
 
 noble.on('scanStart', (result) => {
-  console.log(result)
+  console.log('Scan start: \n', result)
+})
+
+noble.on('scanStop', (result) => {
+  console.log('Scan stop: \n', result)
 })
 
 noble.on('stateChange', (state) => {
-  console.log(state)
+  console.log('State change: \n', state)
+})
+
+noble.on('discover', (result) => {
+  console.log('Discover: \n', result)
+})
+
+noble.on('warning', (result) => {
+  console.log('Warning: \n', result)
 })
